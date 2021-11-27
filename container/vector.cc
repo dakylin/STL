0ea34491849fa3ffc5cad8f42913430dfb9f0adf
Daniel Hu <danielhu83@outlook.com>
2021-11-27 21:36:14 +0800

    Add at method

--- container/vector.cc
+++ container/vector.cc
@@ -9,13 +9,22 @@ int main()
     {
         v.push_back(i);
     }
+
+    std::cout << "visit like array" << std::endl;
     for (int j = 0; j < mxcount; j++)
     {
         std::cout << v[j] << " ";
     }
     std::cout << std::endl;
 
-    std::cout << "Use iterator" << std::endl;
+    std::cout << "visit by at" << std::endl;
+    for (int j = 0; j < mxcount; j++)
+    {
+        std::cout << v.at(j) << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "visit by iterator" << std::endl;
     std::vector<int>::iterator it = v.begin();
     while(it != v.end())
     {
